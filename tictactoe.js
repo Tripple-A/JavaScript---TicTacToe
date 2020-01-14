b10640c5e4b28b10b0d005896e822ef569f001dd
Tripple-A <biodun9@gmail.com>
2020-01-14 19:28:55 +0400

    added the player object constructor and gameboard array

---
+++ tictactoe.js
@@ -0,0 +1,17 @@
+const Player = name =>{
+    return {name}
+}
+
+const game = (() => {
+  let gameboard = [] 
+
+  
+  return{
+      gameboard
+  } 
+   
+  })();
+
+const player1 = Player('Jeff')
+console.log(player1.name)
+console.log(game.gameboard)
