be631eb352e3ed57941f39b41e976b3eff34e2b4
Tripple-A <biodun9@gmail.com>
2020-01-15 16:36:14 +0100

    added the play method

--- tictactoe.js
+++ tictactoe.js
@@ -5,13 +5,16 @@ const Player = name =>{
 const game = (() => {
   let gameboard = [] 
 
-  
+  const play= (tool,position) => {
+    gameboard[position] = tool
+  }
   return{
-      gameboard
+      gameboard , play
   } 
    
   })();
 
 const player1 = Player('Jeff')
 console.log(player1.name)
+game.play('X',5)
 console.log(game.gameboard)
