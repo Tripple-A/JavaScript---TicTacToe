c987adcf7722f5e3da0999b1135a21fcf9416719
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-01-20 19:10:37 +0300

    [fix] Setup HMTL for dom - updated linting config -updated gitignore

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,19 @@
-const Player = name =>{
-    return {name}
-}
+const Player = name => ({ name });
 
 const game = (() => {
-  let gameboard = [] 
+  const gameboard = [];
 
-  const play= (tool,position) => {
-   gameboard[position] == null ? gameboard[position] = tool : console.log('Please,replay: Position picked is occupied')
-  }
-  return{
-      gameboard , play
-  } 
-   
-  })();
+  const play = (tool, position) => {
+    gameboard[position] == null ? gameboard[position] = tool : console.log('Please,replay: Position picked is occupied');
+  };
+  return {
+    gameboard, play,
+  };
+})();
 
-const player1 = Player('Jeff')
-console.log(player1.name)
-game.play('X',5)
-game.play('Y',5)
+const player1 = Player('Jeff');
+console.log(player1.name);
+game.play('X', 5);
+game.play('Y', 5);
 
-console.log(game.gameboard[5])
-
+console.log(game.gameboard[5]);
