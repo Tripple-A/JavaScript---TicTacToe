af9785c5687d89edd503d9d0850a5102690952c1
Tripple-A <biodun9@gmail.com>
2020-01-21 19:14:49 +0100

    added disable button after game is won

--- tictactoe.js
+++ tictactoe.js
@@ -3,48 +3,17 @@ const Player = name =>{
 }
 
 const Player1 = Player('Jeff');
-
 const squares = document.querySelectorAll('.squares')
 let counter = 0
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    document.querySelector('.warn').style.display = 'none'
-    if (square.textContent == '') {
-      counter % 2 == 0? square.textContent = 'X' : square.textContent = 'O'
-      counter += 1
-      game.play(square.textContent , parseInt(square.id - '1',10))
-    if ( Umpire.sayResult() ) { Umpire.endGame()}
-    } else {
-document.querySelector('.warn').style.display = 'block'
-    }
-})
-})
-const Umpire = (() => {
-const sayResult = () => {
-  if (game.check() == 'X') {
-    document.querySelector('.result').innerHTML = `Congratulations ${Player1.name}, You are the winner`
-    return true
-  }
-}
-
-const endGame = () => {
-  squares.forEach(button => {
-    button.disabled = true
-  })
-}
-return {
-  endGame, sayResult
-}
-})();
 
 const game = (() => {
   const gameboard = [];
 
   const check = () => {
-if((gameboard[0] == gameboard[1] && gameboard[0]== gameboard[2]) || 
-  (gameboard[0] == gameboard[3] && gameboard[0]== gameboard[6])) {
+if(gameboard[0] == gameboard[1] && gameboard[0]== gameboard[2]) {
     return gameboard[0]
-    }
+    } 
+    return false
   }
 
   const play = (tool, position) => {
@@ -55,4 +24,40 @@ if((gameboard[0] == gameboard[1] && gameboard[0]== gameboard[2]) ||
    check, gameboard, play 
   };
 })();
+
+const Umpire = (() => {
+  const sayResult = () => {
+    if (game.check() == 'X') {
+      document.querySelector('.result').innerHTML = `Congratulations ${Player1.name}, You are the winner`
+      return true
+    }
+    return false
+  }
+   const endGame = () => {
+    squares.forEach(button => {
+      button.disabled = true
+    })
+  }
+  return {
+    endGame, sayResult
+  }
+  })();
+
+
+squares.forEach(square => {
+  square.addEventListener('click', () => {
+    document.querySelector('.warn').style.display = 'none'
+    if (square.textContent == '') {
+      counter % 2 == 0? square.textContent = 'X' : square.textContent = 'O'
+      counter += 1
+      game.play(square.textContent , parseInt(square.id - '1',10))
+    if (Umpire.sayResult()) {
+       Umpire.endGame() 
+      }
+    } else {
+document.querySelector('.warn').style.display = 'block'
+    }
+})
+})
+
 
