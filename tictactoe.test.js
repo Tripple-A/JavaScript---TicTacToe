6ad49bd3b12929257790f82969cc80c09d5f9c7f
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-03-03 02:25:27 +0300

    feat: add test 1

--- tictactoe.test.js
+++ tictactoe.test.js
@@ -1,4 +1,23 @@
+jest.mock('./tictactoe.js');
 
-const game = require('./tictactoe');
+test('should hide form when data is input', () => {
+  document.body.innerHTML = '<div class="form">'
+        + '<p>'
+        + '<label for="player1"> Player 1 Name</label>'
+        + '<input type="text" class="player1">'
+        + '</p>'
+        + '<p>'
+            + '<label for="player2"> Player 2 Name</label>'
+            + '<input type="text" class="player2">'
+        + '</p>'
+        + '<button id="start">START GAME</button>'
+    + '</div>';
 
-console.log(game);
+  // eslint-disable-next-line global-require
+  const ticTacToe = require('./tictactoe');
+
+  const form = document.querySelector('.form');
+
+  document.getElementById('start').addEventListener('click', ticTacToe.Umpire.restartGame());
+  expect(form.getAttribute('style')).toEqual('display:none;');
+});
